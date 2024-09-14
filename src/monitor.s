d662dec62aadcebfb9320c3a22c7fed654ebda27
Garrettdw304 <garrettdw14497@outlook.com>
2024-09-13 22:04:31 -0400

    write and read command documentation

--- src/monitor.s
+++ src/monitor.s
@@ -1,2 +1,24 @@
+print_cmd_str: .asciiz "print"
+read_cmd_str: .asciiz "read"
+
 begin_monitoring:
-    
+    rts
+
+; Reads a region of memory and writes it to a serial port
+; read <start address> <length> <port>
+; start address: The address of the first byte to print.
+; length: The number of bytes to print.
+; port: The serial port to write to. 0 being the standard io that this
+;  monitor program uses. 1-3 being the other serial ports that
+;  this custom system supports (defined in memory_map.s).
+read_cmd_handler:
+    rts
+; Writes input from a serial port into a region of memory
+; write <start address> <length> <port>
+; start address: The address to store the first byte at.
+; length: The number of bytes to read.
+; port: The serial port to read from. 0 being the standard io that this
+;  monitor program uses. 1-3 being the other serial ports that
+;  this custom system supports (defined in memory_map.s).
+write_cmd_handler:
+    rts
